c79790373b4c54afaf38279a68b1f5e8c8b1cd58
keisyaamalia <kkaysaamalia@gmail.com>
2026-04-06 11:19:15 +0700

    mengisi data

--- struct.cpp
+++ struct.cpp
@@ -12,5 +12,16 @@ int main()
 {
     // deklarasi variable struct
     mahasiswa mhs;
+    // mengisi data
+    cout << "isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "isikan data usia : ";
+    cin >> mhs.umur;
 
+    cout << "data mahasiswa" << endl;
+    cout << "nama : "<< mhs.nama << endl;
+    cout << "alamat : "<< mhs.alamat << endl;
+    cout << "umur : "<< mhs.umur << endl;
 }
