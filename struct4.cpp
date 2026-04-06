c1469bef70631f6c31a4b01b91dd8e32f472f83d
keisyaamalia <kkaysaamalia@gmail.com>
2026-04-06 11:39:43 +0700

    mengisi data

--- struct4.cpp
+++ struct4.cpp
@@ -19,5 +19,25 @@ int main()
 {
     // deklarasi variable struct
     mahasiswa mhs[3];
-    
+    // mengisi data
+    for (int i = 0; i <= 2; i++)
+    {
+    cout << "isikan data nama : ";
+    getline(cin, mhs[i].nama);
+    cout << "isikan data kota : ";
+    getline(cin, mhs[i].alamat.kota);
+    cout << "isikan data desa : ";
+    getline(cin, mhs[i].alamat.desa);
+    cout << "isikan data usia : ";
+    cin >> mhs[i].umur;
+    cin.ignore();
+
+    for (int i = 0; i <= 2; i++)
+    {
+    cout << "data mahasiswa ke-" << i + 1 << " : " << endl;
+    cout << "nama : "<< mhs[i].nama << endl;
+    cout << "alamat : "<< mhs[i].alamat.kota << endl;
+    cout << "alamat : "<< mhs[i].alamat.desa << endl;
+    cout << "umur : "<< mhs[i].umur << endl;
+    }
 }
